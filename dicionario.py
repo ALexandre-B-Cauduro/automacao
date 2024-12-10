0c3bce5164fa9cccb44e3b9cbbc79f51903cdee1
ALexandre-B-Cauduro <alexandre.cauduro@pop-rs.rnp.br>
2024-12-10 08:21:59 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -2,3 +2,4 @@ host_indo={
    'host1':{'hostname1': 'instuição.net.tche.br', 'username': 'login', 'password': 'senha', 'manufacturer' : 'fabricante'},
    'host2':{'hostname2': 'instuição.net.tche.br', 'username': 'login', 'password': 'senha', 'manufacturer' : 'fabricante'}, 
    'host3':{'hostname3': 'instuição.net.tche.br', 'username': 'login', 'password': 'senha', 'manufacturer' : 'fabricante'},
+}
